a51d33648207ce398e90787ac61e8995faf58470
Ummyers <mu.andrea@ciencias.unam.mx>
2018-09-27 20:54:22 -0500

    Actualizando practicas

---
+++ Practica03/Practica03.ino
@@ -0,0 +1,21 @@
+/* Practica 03:
+    Muñiz Patiño Andrea Fernanda
+    23/Agosto/2018
+    Versión 1.0
+    Sobre la práctica:
+    El boton que esta en el microcontrolador se programa para encender un led cuando se presione. 
+    RECORDATORIO: 
+    El método digitalRead solo recibe como parametró el pin que hara la lectura. 
+*/
+
+void setup() {
+  pinMode(5,INPUT_PULLUP); 
+  pinMode(14,OUTPUT);
+}
+
+void loop() {
+  while(!digitalRead(5)){
+    digitalWrite(14,HIGH);    
+  }
+    digitalWrite(14,LOW);  
+}
---
+++ Practica04/PRACTICA04.ino
@@ -0,0 +1,28 @@
+/*Practica 04:
+  * Muñiz Patiño Andrea Fernanda
+  * 24/Agosto/2018
+  * Versión 1.0
+  * Sobre la práctica:
+  * Presionar el botón prender un led, que se quede prendido
+  * Si se vuelve a presionar apagar el botón. 
+  */
+
+boolean prendido;
+
+void setup() {
+  pinMode(5,INPUT_PULLUP); 
+  pinMode(2,OUTPUT);
+}
+void loop() {
+  while(!digitalRead(5)){
+    if(prendido){
+        digitalWrite(2,0);
+      prendido = false;  
+    }else{
+      digitalWrite(2,1); 
+      prendido = true; 
+    }
+  }
+  /*El delay es opcional*/
+  delay(100);
+}
